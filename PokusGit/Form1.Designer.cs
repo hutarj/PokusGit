959c77029943c045c8fd134e5f7e877c98c1e320
hutarj.04 <hutarj.04@A321-11>
2022-03-04 11:26:34 +0100

    Předělání do jednoho buttonu

--- PokusGit/Form1.Designer.cs
+++ PokusGit/Form1.Designer.cs
@@ -28,23 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.vypocti = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 64);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Součet";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -60,47 +47,24 @@
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
             // 
-            // button2
+            // vypocti
             // 
-            this.button2.Location = new System.Drawing.Point(12, 93);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Rozdíl";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(12, 122);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Součin";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(12, 151);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Podíl";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.vypocti.Location = new System.Drawing.Point(12, 64);
+            this.vypocti.Name = "vypocti";
+            this.vypocti.Size = new System.Drawing.Size(100, 23);
+            this.vypocti.TabIndex = 6;
+            this.vypocti.Text = "Vypočti";
+            this.vypocti.UseVisualStyleBackColor = true;
+            this.vypocti.Click += new System.EventHandler(this.vypocti_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(435, 160);
+            this.Controls.Add(this.vypocti);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -109,13 +73,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button vypocti;
     }
 }
 
--- PokusGit/Form1.cs
+++ PokusGit/Form1.cs
@@ -17,35 +17,17 @@ namespace PokusGit
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void vypocti_Click(object sender, EventArgs e)
         {
             double a = int.Parse(textBox1.Text);
             double b = int.Parse(textBox2.Text);
+
             double soucet = a + b;
             MessageBox.Show("Součet je " + soucet.ToString());
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            double a = int.Parse(textBox1.Text);
-            double b = int.Parse(textBox2.Text);
             double rozdil = a - b;
             MessageBox.Show("Rozdíl je " + rozdil.ToString());
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            double a = int.Parse(textBox1.Text);
-            double b = int.Parse(textBox2.Text);
             double soucin = a * b;
             MessageBox.Show("Součin je " + soucin.ToString());
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            double a = int.Parse(textBox1.Text);
-            double b = int.Parse(textBox2.Text);
             if (b != 0)
             {
                 double podil = a / b;
